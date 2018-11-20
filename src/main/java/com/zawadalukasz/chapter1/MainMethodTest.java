696a254431ea549d0053ba2e4b23897c4ee60d54
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-20 18:33:05 +0100

    Tested new operator and equals

---
+++ src/main/java/com/zawadalukasz/chapter1/MainMethodTest.java
@@ -0,0 +1,10 @@
+package com.zawadalukasz.chapter1;
+
+import java.awt.*;
+
+public class MainMethodTest {
+    //Main with capital M does not compile -> Error: Main method not found in class
+//    public static void Main(String[] fsda) {
+//        System.out.println("Main works");
+//    }
+}
