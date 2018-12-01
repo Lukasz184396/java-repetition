2fd4795bd075f2965f4c6b05d53936c8b1f47776
Lukasz184396 <zawadalukasz88@gmail.com>
2018-12-01 11:45:53 +0100

    Printing null example

---
+++ src/main/java/com/zawadalukasz/chapter1/PrintingNullExample.java
@@ -0,0 +1,10 @@
+package com.zawadalukasz.chapter1;
+
+public class PrintingNullExample {
+    public static void main(String[] args) {
+        String stringNull = null;
+        System.out.println(stringNull);     //null
+        //System.out.println(null);           //not compile ambiguous
+    }
+}
+
