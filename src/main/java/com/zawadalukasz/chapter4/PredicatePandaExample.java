50cef1a082856fe80b4051298674f385047cd7f0
Lukasz184396 <zawadalukasz88@gmail.com>
2018-12-04 18:43:45 +0100

    Adde Panda<Predicate> example with Lambda

---
+++ src/main/java/com/zawadalukasz/chapter4/PredicatePandaExample.java
@@ -0,0 +1,23 @@
+package com.zawadalukasz.chapter4;
+
+
+import java.util.function.Predicate;
+
+public class PredicatePandaExample {
+
+}
+
+class Panda {
+    int age;
+
+    public static void main(String[] args) {
+        Panda panda = new Panda();
+        panda.age = 1;
+        check(panda, p -> p.age <5);
+    }
+
+    private static void check(Panda panda, Predicate<Panda> pandaPredicate) {
+        String result = pandaPredicate.test(panda) ? "match" : "not match";
+        System.out.println(result);
+    }
+}
