c07e08cfb9a1af75e00accf67675b68ee4558d52
Lukasz184396 <zawadalukasz88@gmail.com>
2018-12-04 18:02:30 +0100

    Fix of comment in StringBuilderAsParamExample

--- src/main/java/com/zawadalukasz/chapter4/StringBuilderAsParamExample.java
+++ src/main/java/com/zawadalukasz/chapter4/StringBuilderAsParamExample.java
@@ -16,7 +16,7 @@ public class StringBuilderAsParamExample {
 
         System.out.println("s1 = " + s1 );      //s1
         System.out.println("s2 = " + s2 );      //s2b
-        System.out.println("s3 = " + s3 );      //s3
+        System.out.println("s3 = " + s3 );      //a
 
     }
 }
